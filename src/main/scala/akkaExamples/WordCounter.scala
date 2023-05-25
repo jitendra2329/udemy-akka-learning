8f376b92cd1d793d4599d493406287c911ee3df6
Jitendra <jitendra.kumar@knoldus.com>
2023-05-25 14:36:02 +0530

    Add actor with arguments with constructor.
    An actor is created with arguments and it is being instantiated by creating campanion object of the actor.

--- src/main/scala/akkaExamples/WordCounter.scala
+++ src/main/scala/akkaExamples/WordCounter.scala
@@ -2,7 +2,7 @@ package akkaExamples
 
 import akka.actor.{Actor, ActorSystem, Props}
 
-object WordCounter extends App{
+object WordCounter extends App {
 
   // step 1 : actor system
   val actorSystem = ActorSystem("actorSystem")
@@ -11,7 +11,8 @@ object WordCounter extends App{
   private class WordCounter extends Actor {
 
     private var wordCount = 0
-    def receive:  PartialFunction[Any, Unit] = {
+
+    def receive: PartialFunction[Any, Unit] = {
       case message: String =>
         println(s"I have received message: $message")
         wordCount += message.split(" ").length
@@ -22,8 +23,30 @@ object WordCounter extends App{
 
   // step instantiating the actor
 
-  private val wordCounter = actorSystem.actorOf(Props[WordCounter],"wordCounter")
+  private val wordCounter = actorSystem.actorOf(Props[WordCounter], "wordCounter")
 
   wordCounter ! "My name is Jitendra and I am learning Akka."
+
+
+  // actor with constructor arguments
+
+
+  private object Person {
+    def props(name: String): Props = Props(new Person(name))
+  }
+
+  private class Person(name: String) extends Actor {
+    override def receive: Receive = {
+      case "hi" => println(s"Hi, my name is $name .")
+      case _ => println("Undefined message.")
+    }
+  }
+
+  // creating actor with constructor arguments in this is not recommended.
+  //  private val person = actorSystem.actorOf(Props(new Person("Jitendra")))
+
+  // best practice to do above is by creating the companion object
+  private val person = actorSystem.actorOf(Person.props("Jitendra"))
+  person ! "hi"
 
 }
