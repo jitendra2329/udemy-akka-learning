8cbeb67ab3d84fad1433f443a98154125e89d2af
Jitendra <jitendra.kumar@knoldus.com>
2023-05-25 17:55:00 +0530

    Counter code modified, The messages(Incremet, Decrement and Print) are kept into the campanion object

--- src/main/scala/akkaExamples/CounterActor.scala
+++ src/main/scala/akkaExamples/CounterActor.scala
@@ -4,21 +4,33 @@ import akka.actor.{Actor, ActorSystem, Props}
 
 object CounterActor extends App {
 
+  private object Counter {
+    case object Increment
+
+    case object Decrement
+
+    case object Print
+  }
+
   private class Counter extends Actor {
+
+    import Counter._
+
     private var count = 0
+
     override def receive: Receive = {
-      case "Increment" => count += 1
-      case "Decrement" => count -= 1
-      case "Print" => println(s"Current value of the COUNTER : $count")
+      case Increment => count += 1
+      case Decrement => count -= 1
+      case Print => println(s"Current value of the COUNTER : $count")
     }
   }
 
   val system = ActorSystem("counterActor")
   private val counter = system.actorOf(Props[Counter], "counter")
-  counter ! "Increment"
-  counter ! "Increment"
-  counter ! "Increment"
-  counter ! "Increment"
-  counter ! "Decrement"
-  counter ! "Print"
+
+  import Counter._
+
+  (1 to 10).foreach(_ => counter ! Increment)
+  (1 to 4).foreach(_ => counter ! Decrement)
+  counter ! Print
 }
