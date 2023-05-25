f7b15c30a789d8dbfd4d4ddf0498171d70279dcb
Jitendra <jitendra.kumar@knoldus.com>
2023-05-25 15:48:44 +0530

    Add message forwarding and communication beween actors.

--- src/main/scala/akkaExamples/ActorCapabilities.scala
+++ src/main/scala/akkaExamples/ActorCapabilities.scala
@@ -1,13 +1,17 @@
 package akkaExamples
 
-import akka.actor.{Actor, ActorSystem, Props}
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 
 object ActorCapabilities extends App {
   private class SimpleActor extends Actor {
     override def receive: Receive = {
-      case message: String => println(s"I have received a MESSAGE : $message")
+      case "Hii" => sender() ! "Hello, how are you?"
+      case message: String => println(s" $self I have received a MESSAGE : $message")
       case number: Int => println(s"I have received a NUMBER : $number")
       case SpecialMessage(content) => println(s"I have received a special MESSAGE : $content")
+      case SendMessageToYourSelf(content) => self ! content
+      case SayHiTo(ref) => ref ! "Hii"
+      case ForwardMessage(msg, ref) => ref forward msg // I keep the original sender of the message
     }
   }
 
@@ -20,4 +24,29 @@ object ActorCapabilities extends App {
   actor ! SpecialMessage("I am learning Akka from ROCK THE JVM.")
 
   private case class SpecialMessage(msg: String)
+
+
+  // 2. Actors have information about the context and themselves
+
+  private case class SendMessageToYourSelf(message: String)
+
+  actor ! SendMessageToYourSelf("I am an actor and i am proud of it.")
+
+  // 3. actor can REPLY to message
+
+  private val jeet = system.actorOf(Props[SimpleActor], "jeet")
+  private val manish = system.actorOf(Props[SimpleActor], "manish")
+
+  private case class SayHiTo(ref: ActorRef)
+
+  manish ! SayHiTo(jeet)
+
+  // 4. dead letters (The messages which are not delivered to any actors, are goes to the DEAD LETTERS)
+  // dead letters is a fake actor in Akka.
+  manish ! "Hii"
+
+  // 5. forwarding the message (by keeping the original sender reference)
+  private case class ForwardMessage(message: String, ref: ActorRef)
+
+  manish ! ForwardMessage("hello", jeet)
 }
